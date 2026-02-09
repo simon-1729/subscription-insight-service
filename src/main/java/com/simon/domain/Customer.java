7c683387bdf69d956000e09ec586e7bdfce886ec
CodePeeler <sdornan1729@gmail.com>
2026-02-09 18:16:54 +0000

    Added Domain classes; Customer, Subscription

---
+++ src/main/java/com/simon/domain/Customer.java
@@ -0,0 +1,32 @@
+package com.simon.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/simon/domain/Subscription.java
@@ -0,0 +1,39 @@
+package com.simon.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subscription {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    private String planType;
+    private String status;
+
+    private LocalDate startDate;
+    private LocalDate renewalDate;
+}
